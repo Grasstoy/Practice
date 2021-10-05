70c704859100bff7443a8c5ad02ab8eeb3007b98
Grasstoy <91662910+Grasstoy@users.noreply.github.com>
2021-10-06 00:46:24 +0900

    Update ProjectEuler_Problem2.py

--- ProjectEuler_Problem2.py
+++ ProjectEuler_Problem2.py
@@ -1,14 +1,14 @@
 # 4백만 이하의 짝수 값을 갖는 모든 피보나치 항을 더하면 얼마가 될까?
-pibo_list = [1, 2]
+fibo_list = [1, 2]
 x = 0
 num = 1
 
-while pibo_list[num] < 3000000: # 이 부분이 약간 틀린 것 같 
-    value = pibo_list[num-1] + pibo_list[num]
+while fibo_list[num] < 3000000: # 이 부분이 약간 틀린 것 같 
+    value = pibo_list[num-1] + fibo_list[num]
     num += 1
     pibo_list.append(value)
 
-for y in pibo_list:
+for y in fibo_list:
     if y % 2 == 0:
         x += y
 
